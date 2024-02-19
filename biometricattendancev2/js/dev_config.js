1738a8e06a3234470aacf6dec7e87db40adc50f0
Naren Kaniyan <39701147+NarenKaniyan@users.noreply.github.com>
2024-02-19 16:44:42 +0530

    Update dev_config.js

--- biometricattendancev2/js/dev_config.js
+++ biometricattendancev2/js/dev_config.js
@@ -18,25 +18,34 @@ $(document).ready(function(){
 		    $('#dev_name').val('');
 		    $('#dev_dep').val('');
 
-		    if (response == 1) {			    
-		    	$('.alert_dev').fadeIn(500);
-			    $('.alert_dev').html('<p class="alert alert-success">A new device has been added successfully</p>');
-		        $('#new-device').modal('hide');
-		        setTimeout(function () {
-			        $('.alert_dev').fadeOut(500);
-			        
-			    }, 2000);
-		    }
-		    else {
-	    		$('.alert_dev').fadeIn(500);
-		    	$('.alert_dev').html(response);
+			  // Cache the jQuery selectors
+			  var $alertDev = $('.alert_dev');
+			  var $newDeviceModal = $('#new-device');
 
-		    	setTimeout(function () {
-			        $('.alert_dev').fadeOut(500);
-			    }, 2000);
-		    }
+			  // Cache the jQuery selectors
+			  var $alertDev = $('.alert_dev');
 
-		    $.ajax({
+			  if (response === 1) {
+				  // Use the cached selectors
+				  $alertDev.fadeIn(500);
+				  $alertDev.html('<p class="alert alert-success">A new device has been added successfully</p>');
+				  $('#new-device').modal('hide');
+
+				  setTimeout(function () {
+					  $alertDev.fadeOut(500);
+				  }, 2000);
+			  } else {
+				  // Use the cached selectors
+				  $alertDev.fadeIn(500);
+				  $alertDev.html(response);
+
+				  setTimeout(function () {
+					  $alertDev.fadeOut(500);
+				  }, 2000);
+			  }
+
+
+			  $.ajax({
 		      url: "dev_up.php",
 		      type: 'POST',
 		      data: {
@@ -71,33 +80,39 @@ $(document).ready(function(){
 			        $(el).closest('tr').fadeOut(300,function(){
 			              $(this).show();
 			        });
-			        if(response == 1){			    
-				    	$('.alert_dev').fadeIn(500);
-			        	$('.alert_dev').html('<p class="alert alert-success">The device Token has been updated successfully</p>');
+					   // Cache the jQuery selector
+					   var $alertDev = $('.alert_dev');
 
-				        setTimeout(function () {
-					        $('.alert_dev').fadeOut(500);
-					        
-					    }, 2000);
-			            $.ajax({
-			              	url: "dev_up.php",
-			              	type: 'POST',
-			              	data: {
-			                  'dev_up': 1,
-			              	}
-			              	}).done(function(data) {
-			              	$('#devices').html(data);
-			            });
-			        }
-				    else {
-			    		$('.alert_dev').fadeIn(500);
-				    	$('.alert_dev').html(response);
+					   if (response === 1) {
+						   // Use the cached selector
+						   $alertDev.fadeIn(500);
+						   $alertDev.html('<p class="alert alert-success">The device Token has been updated successfully</p>');
 
-				    	setTimeout(function () {
-					        $('.alert_dev').fadeOut(500);
-					    }, 2000);
-				    }
-			       }
+						   setTimeout(function () {
+							   $alertDev.fadeOut(500);
+						   }, 2000);
+
+						   // Make the AJAX call
+						   $.ajax({
+							   url: "dev_up.php",
+							   type: 'POST',
+							   data: {
+								   'dev_up': 1,
+							   }
+						   }).done(function(data) {
+							   $('#devices').html(data);
+						   });
+					   } else {
+						   // Use the cached selector
+						   $alertDev.fadeIn(500);
+						   $alertDev.html(response);
+
+						   setTimeout(function () {
+							   $alertDev.fadeOut(500);
+						   }, 2000);
+					   }
+
+				   }
 			     });
 		   	}
 		});
@@ -122,32 +137,40 @@ $(document).ready(function(){
 		       success: function(response){
 
 		         // Removing row from HTML Table
-		          if(response == 1){
-		              $(el).closest('tr').css('background','#d9534f');
-		              $(el).closest('tr').fadeOut(800,function(){
-		                $(this).remove();
-		              });
+				   // Cache the jQuery selectors
+				   var $alertDevDel = $('.alert_dev_del');
+				   var $closestTr = $(el).closest('tr');
 
-		              $.ajax({
-	                    url: "dev_up.php",
-	                    type: 'POST',
-	                    data: {
-	                        'dev_up': 1,
-	                    }
-	                    }).done(function(data) {
-	                    $('#devices').html(data);
-	                  });
-		          }
-		          else{
-		            $('.alert_dev_del').fadeIn(500);
-		            $('.alert_dev_del').html(response);
+				   if (response === 1) {
+					   // Apply styling and perform fadeout animation on the closest tr
+					   $closestTr.css('background', '#d9534f');
+					   $closestTr.fadeOut(800, function() {
+						   $(this).remove();
+					   });
 
-		            setTimeout(function () {
-		                $('.alert_dev_del').fadeOut(500);
-		            }, 2000);
-		              bootbox.alert('Device not deleted.');
-		          }
-		       }
+					   // Make the AJAX call
+					   $.ajax({
+						   url: "dev_up.php",
+						   type: 'POST',
+						   data: {
+							   'dev_up': 1,
+						   }
+					   }).done(function(data) {
+						   $('#devices').html(data);
+					   });
+				   } else {
+					   // Show alert and bootbox
+					   $alertDevDel.fadeIn(500);
+					   $alertDevDel.html(response);
+
+					   setTimeout(function () {
+						   $alertDevDel.fadeOut(500);
+					   }, 2000);
+
+					   bootbox.alert('Device not deleted.');
+				   }
+
+			   }
 		     });
 		   }
 		});
@@ -172,34 +195,41 @@ $(document).ready(function(){
 			          'dev_id': dev_id,
 			       },
 			       success: function(response){
-			       		
-			          	if(response == 1){
-			          		// bootbox.alert('<p class="alert alert-success">The device Mode has been updated successfully</p>');
-			              	$(el).closest('tr').css('background','#5cb85c');
-					        $(el).closest('tr').fadeOut(300,function(){
-					              $(this).show();
-					        });
 
-			              	$.ajax({
-								url: "dev_up.php",
-		                    	type: 'POST',
-		                    	data: {
-		                        'dev_up': 1,
-		                    	}
-		                    }).done(function(data) {
-		                    	$('#devices').html(data);
-		                  	});
-			          	}
-			          	else{
-				            $('.alert_dev_del').fadeIn(500);
-				            $('.alert_dev_del').html(response);
+					   // Cache the jQuery selectors
+					   var $alertDevDel = $('.alert_dev_del');
+					   var $closestTr = $(el).closest('tr');
 
-				            setTimeout(function () {
-				                $('.alert_dev_del').fadeOut(500);
-				            }, 2000);
-				              bootbox.alert('Device not changed.');
-		         	 	}
-			       	}
+					   if (response === 1) {
+						   // Apply styling and perform fadeout animation on the closest tr
+						   $closestTr.css('background', '#5cb85c');
+						   $closestTr.fadeOut(300, function() {
+							   $(this).show();
+						   });
+
+						   // Make the AJAX call
+						   $.ajax({
+							   url: "dev_up.php",
+							   type: 'POST',
+							   data: {
+								   'dev_up': 1,
+							   }
+						   }).done(function(data) {
+							   $('#devices').html(data);
+						   });
+					   } else {
+						   // Show alert and bootbox
+						   $alertDevDel.fadeIn(500);
+						   $alertDevDel.html(response);
+
+						   setTimeout(function () {
+							   $alertDevDel.fadeOut(500);
+						   }, 2000);
+
+						   bootbox.alert('Device not changed.');
+					   }
+
+				   }
 			     });
 		   	}
 		   	else{
